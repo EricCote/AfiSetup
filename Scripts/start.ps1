b5d1363f4eda61f2c44c1eabe34748ff7314247d
Eric Cote <eric@coteexpert.com>
2015-10-06 17:12:42 -0400

    gain

--- Scripts/start.ps1
+++ Scripts/start.ps1
@@ -43,3 +43,5 @@ tzutil /s "Eastern Standard Time"
 Set-ItemProperty -Path HKLM:\Software\Microsoft\Windows\CurrentVersion\policies\system -Name EnableLUA -Value 0
 Set-AutoLogon -loginName '.\afi' -password 'afi12345!' -count 5 
 Set-ItemProperty -path "HKLM:\Software\Microsoft\Windows\CurrentVersion\RunOnce" -name "myScript" -value ('powershell -ExecutionPolicy bypass -f "' + (Join-Path (Get-ScriptPath) "newMachineConfig.ps1" ) + '"')
+
+shutdown -r -t 60
