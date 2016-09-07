c3117061d26c529c0f279f61ed6a290e059f47f2
Eric Cote <eric@coteexpert.com>
2016-09-07 17:27:01 -0400

    Mostly install-SqlLocalDb

--- Scripts/Get-AdventureWorks.ps1
+++ Scripts/Get-AdventureWorks.ps1
@@ -13,6 +13,17 @@ function detect-localdb
 
 
 
+function Get-ServerName
+{
+    $svr=""
+    if((Get-ItemPropertyValue -ErrorAction Ignore "HKLM:\SOFTWARE\Microsoft\Microsoft SQL Server\Instance Names\SQL\" "MSSQLSERVER").Length -gt  0)
+    { $svr="." }
+    elseif  ((Get-childItem -ErrorAction Ignore -Path "HKLM:\SOFTWARE\Microsoft\Microsoft SQL Server Local DB\Installed Versions\").Length -gt 0) 
+    { $svr="(localdb)\MSSQLLocalDB" }
+
+    return $svr
+}
+
 
 function Download-FromEdge
 {
@@ -70,11 +81,8 @@ function Run-Sql
     if (test-path "C:\Program Files\Microsoft SQL Server\Client SDK\ODBC\130\Tools\Binn\SQLCMD.EXE")
     {$sqlcmd="C:\Program Files\Microsoft SQL Server\Client SDK\ODBC\130\Tools\Binn\SQLCMD.EXE";};
 
-    $svr=""
-    if((Get-ItemPropertyValue -ErrorAction Ignore "HKLM:\SOFTWARE\Microsoft\Microsoft SQL Server\Instance Names\SQL\" "MSSQLSERVER").Length -gt  0)
-    { $svr="." }
-    else
-    { $svr="(localdb)\MSSQLLocalDB" }
+    $svr=Get-ServerName
+
 
     return & $sqlcmd -S $svr -E -Q $SqlString
 }
@@ -136,169 +144,179 @@ function Enable-AutomaticDownloadEdge
 }
 
 
-
-#start localDB
-& "C:\Program Files\Microsoft SQL Server\130\Tools\Binn\SqlLocalDB.exe" start 
-& "C:\Program Files\Microsoft SQL Server\130\Tools\Binn\SqlLocalDB.exe" info mssqllocaldb
-
-#get codeplex-Version
-$codeplexVersion= Get-CodeplexVersion
-
-
-
-###------------------------------------------------------
-
-$FileNameAW2014=Join-path $dl  "Adventure Works 2014 Full Database Backup.zip"
-
-Download-File "http://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=msftdbprodsamples&DownloadId=880661&FileTime=130507138100830000&Build=$codeplexVersion" $FileNameAW2014
-
-add-type -AssemblyName System.IO.Compression.FileSystem
-[system.io.compression.zipFile]::ExtractToDirectory($FileNameAW2014,'c:\aw\')
-
-
-$cmd="
-RESTORE DATABASE AdventureWorks2014
-  FROM DISK = 'C:\AW\AdventureWorks2014.bak'
-WITH   
-  MOVE 'AdventureWorks2014_Data' 
-  TO 'C:\AW\AdventureWorks_data.mdf', 
-  MOVE 'AdventureWorks2014_Log' 
-  TO 'C:\AW\AdventureWorks_log.ldf';
-"
-
-run-sql $cmd
-
-
-
-###----------------------------------------------------
-
-
-$FileNameAWDW2014=Join-path $dl  "Adventure Works DW 2014 Full Database Backup.zip"
-
-Download-File "http://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=msftdbprodsamples&DownloadId=880664&FileTime=130511246406570000&Build=$codeplexVersion" $FileNameAWDW2014
-
-
-add-type -AssemblyName System.IO.Compression.FileSystem
-[system.io.compression.zipFile]::ExtractToDirectory($FileNameAWDW2014,'c:\aw\')
-
-
-
-$cmd="
-RESTORE DATABASE AdventureWorksDW2014
-  FROM DISK = 'C:\AW\AdventureWorksDW2014.bak'
-WITH   
-  MOVE 'AdventureWorksDW2014_Data' 
-  TO 'C:\AW\AdventureWorksDW_data.mdf', 
-  MOVE 'AdventureWorksDW2014_Log' 
-  TO 'C:\AW\AdventureWorksDW_log.ldf';
-"
-
-run-sql $cmd
-
-
-###-------------------------------------------------------------------------------
-
-$FileNameAWLT2012 = Join-path $dl "AdventureWorksLT2012_Data.mdf";
-
-Download-File  "http://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=msftdbprodsamples&DownloadId=354847&FileTime=129764108568330000&Build=$codeplexVersion" $FileNameAWLT2012
-
-Copy-Item  -Path $FileNameAWLT2012 -Destination 'c:\aw\'
-
-$cmd="
-CREATE DATABASE AdventureWorksLT2012 ON 
-( FILENAME = N'C:\aw\AdventureWorksLT2012_Data.mdf' )
- FOR ATTACH_REBUILD_LOG  ;
-"
-
-run-sql $cmd
-
-
-###-------------------------------------------------------------------------------
-
-
-if (get-sqlYear -get 2016)
+if (Get-ServerName -neq '')
 {
-  $SqlFeature="Standard"
-  if(("Enterprise","Developer") -contains (Get-SqlEdition))
-  { $SqlFeature="Full" }
+
+    if (Get-ServerName -eq '(localdb)\MSSQLLocalDB')
+    {
+        #start localDB
+        & "C:\Program Files\Microsoft SQL Server\130\Tools\Binn\SqlLocalDB.exe" start 
+        & "C:\Program Files\Microsoft SQL Server\130\Tools\Binn\SqlLocalDB.exe" info mssqllocaldb
+
+    }
+
+
+
+
+    #get codeplex-Version
+    $codeplexVersion= Get-CodeplexVersion
+
+
+
+    ###------------------------------------------------------
+
+    $FileNameAW2014=Join-path $dl  "Adventure Works 2014 Full Database Backup.zip"
+
+    Download-File "http://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=msftdbprodsamples&DownloadId=880661&FileTime=130507138100830000&Build=$codeplexVersion" $FileNameAW2014
+
+    add-type -AssemblyName System.IO.Compression.FileSystem
+    [system.io.compression.zipFile]::ExtractToDirectory($FileNameAW2014,'c:\aw\')
+
+
+    $cmd="
+    RESTORE DATABASE AdventureWorks2014
+      FROM DISK = 'C:\AW\AdventureWorks2014.bak'
+    WITH   
+      MOVE 'AdventureWorks2014_Data' 
+      TO 'C:\AW\AdventureWorks_data.mdf', 
+      MOVE 'AdventureWorks2014_Log' 
+      TO 'C:\AW\AdventureWorks_log.ldf';
+    "
+
+    run-sql $cmd
+
+
+
+    ###----------------------------------------------------
+
+
+    $FileNameAWDW2014=Join-path $dl  "Adventure Works DW 2014 Full Database Backup.zip"
+
+    Download-File "http://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=msftdbprodsamples&DownloadId=880664&FileTime=130511246406570000&Build=$codeplexVersion" $FileNameAWDW2014
+
+
+    add-type -AssemblyName System.IO.Compression.FileSystem
+    [system.io.compression.zipFile]::ExtractToDirectory($FileNameAWDW2014,'c:\aw\')
+
+
+
+    $cmd="
+    RESTORE DATABASE AdventureWorksDW2014
+      FROM DISK = 'C:\AW\AdventureWorksDW2014.bak'
+    WITH   
+      MOVE 'AdventureWorksDW2014_Data' 
+      TO 'C:\AW\AdventureWorksDW_data.mdf', 
+      MOVE 'AdventureWorksDW2014_Log' 
+      TO 'C:\AW\AdventureWorksDW_log.ldf';
+    "
+
+    run-sql $cmd
+
+
+    ###-------------------------------------------------------------------------------
+
+    $FileNameAWLT2012 = Join-path $dl "AdventureWorksLT2012_Data.mdf";
+
+    Download-File  "http://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=msftdbprodsamples&DownloadId=354847&FileTime=129764108568330000&Build=$codeplexVersion" $FileNameAWLT2012
+
+    Copy-Item  -Path $FileNameAWLT2012 -Destination 'c:\aw\'
+
+    $cmd="
+    CREATE DATABASE AdventureWorksLT2012 ON 
+    ( FILENAME = N'C:\aw\AdventureWorksLT2012_Data.mdf' )
+     FOR ATTACH_REBUILD_LOG  ;
+    "
+
+    run-sql $cmd
+
+
+    ###-------------------------------------------------------------------------------
+
+
+    if (get-sqlYear -get 2016)
+    {
+      $SqlFeature="Standard"
+      if(("Enterprise","Developer") -contains (Get-SqlEdition))
+      { $SqlFeature="Full" }
  
 
-Download-File ("https://github.com/Microsoft/sql-server-samples/releases/download/wide-world-importers-v1.0/WideWorldImporters-" + $SqlFeature + ".bak")  (Join-path $dl  ("WideWorldImporters-" + $SqlFeature + ".bak"))
+    Download-File ("https://github.com/Microsoft/sql-server-samples/releases/download/wide-world-importers-v1.0/WideWorldImporters-" + $SqlFeature + ".bak")  (Join-path $dl  ("WideWorldImporters-" + $SqlFeature + ".bak"))
 
 
 
-Copy-Item  -Path (Join-path $dl  "WideWorldImporters-*.bak") -Destination 'c:\aw\'
+    Copy-Item  -Path (Join-path $dl  "WideWorldImporters-*.bak") -Destination 'c:\aw\'
 
-$part=""
-if ($SqlFeature -eq "Full") 
-{  $part = " MOVE 'WWI_InMemory_Data_1' TO 'c:\AW\WideWorldImporters_InMemory_Data_1', " };
+    $part=""
+    if ($SqlFeature -eq "Full") 
+    {  $part = " MOVE 'WWI_InMemory_Data_1' TO 'c:\AW\WideWorldImporters_InMemory_Data_1', " };
 
-$cmd="
-RESTORE DATABASE WideWorldImporters
-  FROM DISK = 'C:\AW\WideWorldImporters-" +  $SqlFeature  +  ".bak'
-WITH   
-  MOVE 'WWI_Primary' 
-  TO 'C:\AW\WideWorldImporters.mdf', 
-  MOVE 'WWI_UserData' 
-  TO 'C:\AW\WideWorldImporters_UserData.ndf',
-"  + $part + "
-  MOVE 'WWI_Log' 
-  TO 'C:\AW\WideWorldImporters.ldf';
-"
-
-
-Run-Sql $cmd
+    $cmd="
+    RESTORE DATABASE WideWorldImporters
+      FROM DISK = 'C:\AW\WideWorldImporters-" +  $SqlFeature  +  ".bak'
+    WITH   
+      MOVE 'WWI_Primary' 
+      TO 'C:\AW\WideWorldImporters.mdf', 
+      MOVE 'WWI_UserData' 
+      TO 'C:\AW\WideWorldImporters_UserData.ndf',
+    "  + $part + "
+      MOVE 'WWI_Log' 
+      TO 'C:\AW\WideWorldImporters.ldf';
+    "
 
 
-
-###-------------------------------------------------------------------------------
+    Run-Sql $cmd
 
 
 
+    ###-------------------------------------------------------------------------------
 
 
-Download-File "https://github.com/Microsoft/sql-server-samples/releases/download/wide-world-importers-v1.0/WideWorldImportersDW-$SqlFeature.bak" (Join-path $dl "WideWorldImportersDW-$SqlFeature.bak")
 
 
-Copy-Item  -Path (Join-path $dl  "WideWorldImportersDW-*.bak") -Destination 'c:\aw\'
 
-$part=""
-if ($SqlFeature -eq "Full") 
-{  $part =  " MOVE 'WWIDW_InMemory_Data_1' TO 'c:\AW\WideWorldImportersDW_InMemory_Data_1', "  };
+    Download-File "https://github.com/Microsoft/sql-server-samples/releases/download/wide-world-importers-v1.0/WideWorldImportersDW-$SqlFeature.bak" (Join-path $dl "WideWorldImportersDW-$SqlFeature.bak")
 
 
-$cmd="
-RESTORE DATABASE WideWorldImportersDW
-  FROM DISK = 'C:\AW\WideWorldImportersDW-" +  $SqlFeature  +  ".bak'
-WITH   
-  MOVE 'WWI_Primary' 
-  TO 'C:\AW\WideWorldImportersDW.mdf', 
-  MOVE 'WWI_UserData' 
-  TO 'C:\AW\WideWorldImportersDW_UserData.ndf',
-"  + $part + "
-  MOVE 'WWI_Log' 
-  TO 'C:\AW\WideWorldImportersDW.ldf';
-"
+    Copy-Item  -Path (Join-path $dl  "WideWorldImportersDW-*.bak") -Destination 'c:\aw\'
+
+    $part=""
+    if ($SqlFeature -eq "Full") 
+    {  $part =  " MOVE 'WWIDW_InMemory_Data_1' TO 'c:\AW\WideWorldImportersDW_InMemory_Data_1', "  };
 
 
-run-sql $cmd
+    $cmd="
+    RESTORE DATABASE WideWorldImportersDW
+      FROM DISK = 'C:\AW\WideWorldImportersDW-" +  $SqlFeature  +  ".bak'
+    WITH   
+      MOVE 'WWI_Primary' 
+      TO 'C:\AW\WideWorldImportersDW.mdf', 
+      MOVE 'WWI_UserData' 
+      TO 'C:\AW\WideWorldImportersDW_UserData.ndf',
+    "  + $part + "
+      MOVE 'WWI_Log' 
+      TO 'C:\AW\WideWorldImportersDW.ldf';
+    "
+
+
+    run-sql $cmd
+
+
+    }
+
+
+
+    #####---------------------------------------------------------------------------------------------
+
+
+    del $FileNameAW2014
+    del $FileNameAWDW2014
+    del $FileNameAWLT2012
+    del (Join-path $dl  "WideWorldImporters*.bak") 
 
 
 }
-
-
-
-#####---------------------------------------------------------------------------------------------
-
-
-del $FileNameAW2014
-del $FileNameAWDW2014
-del $FileNameAWLT2012
-del (Join-path $dl  "WideWorldImporters*.bak") 
- 
-
-
-
-
-
+else
+{
+    "No SQL Server detected!"
+}
 
--- Scripts/install-scriptBrowser.ps1
+++ Scripts/install-scriptBrowser.ps1
@@ -1,12 +1,13 @@
 ﻿
 
-Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser
-
+#Set-ExecutionPolicy -ExecutionPolicy undefined -Scope CurrentUser
+Get-ExecutionPolicy -list
 
 
 Get-PackageProvider -ListAvailable 
+Get-PackageSource 
 
-Install-PackageProvider -Name NuGet 
+Install-PackageProvider -Name NuGet -force
 Import-PackageProvider -Name  NuGet 
 Install-Module ScriptBrowser –Scope CurrentUser
 Import-Module ScriptBrowser
@@ -14,6 +15,8 @@ Import-Module ScriptBrowser
 
 
 Get-PackageProvider -ListAvailable 
+
+
 
 
 Enable-ScriptBrowser
